c34ca057ce83b31446307342bb7f880148d68650
frizzle101101 <aaronfritz96@gmail.com>
2024-11-04 15:20:42 -0500

    Commit Solution

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿namespace _4_Median_of_Two_Sorted_Arrays {
+    internal class Program {
+        static void Main(string[] args) {
+            Solution solution = new Solution();
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
+        var nums = nums1.Concat(nums2).ToArray();
+        Array.Sort(nums);
+        var length = nums.Length;
+        if (length > 0 && length % 2 == 0) {
+            //even
+            return (nums[length / 2] + nums[length / 2 - 1]) / 2.0;
+        } else {
+            //odd
+            return nums[length / 2];
+        }
+    }
+}
